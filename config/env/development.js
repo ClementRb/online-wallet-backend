0bfd72bd67eda4e654236bc5ff3300d135975330
Clément <clement.ribourt@gmail.com>
2021-02-19 16:11:06 +0100

    Update port

--- config/env/development.js
+++ config/env/development.js
@@ -2,7 +2,7 @@ export default {
     env: 'development',
     db:
         'mongodb+srv://online-wallet-admin:xiG6YLNl91WdQPzX@online-wallet-cluster.nw57q.mongodb.net/online-wallet-db?retryWrites=true&w=majority',
-    port: 3000,
+    port: 5000,
     jwtSecret: 'my-api-secret',
     jwtDuration: '2 hours',
 };
