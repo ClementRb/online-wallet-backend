9ba4b9bb77c431aa546a177c95abc17f91b65b51
Clément <clement.ribourt@gmail.com>
2021-04-22 10:59:06 +0200

    localdb

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,6 @@
 export default {
     env: 'development',
-    db:
-        'mongodb+srv://online-wallet-admin:xiG6YLNl91WdQPzX@online-wallet-cluster.nw57q.mongodb.net/online-wallet-db?retryWrites=true&w=majority',
+    db: 'mongodb://localhost/postgres',
     port: 5000,
     jwtSecret: 'my-api-secret',
 };
